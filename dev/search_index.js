5874bb63a76127bf3f48f79ec1e85a4ca3ebacc8
Documenter.jl <documenter@juliadocs.github.io>
2023-10-16 21:47:41 +0000

    build based on 273c1de

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = AtomsCalculators","category":"page"},{"location":"#AtomsCalculators","page":"Home","title":"AtomsCalculators","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for AtomsCalculators.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [AtomsCalculators]","category":"page"}]
+}
