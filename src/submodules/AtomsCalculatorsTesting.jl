9553f0ad189c8fd45aa59d3bcce5cb929b097b08
Teemu Järvinen <teemu.j.jarvinen@gmail.com>
2023-12-08 18:37:19 -0800

    Specify that non allocating forces are added

--- src/submodules/AtomsCalculatorsTesting.jl
+++ src/submodules/AtomsCalculatorsTesting.jl
@@ -33,6 +33,8 @@ function test_forces(sys, calculator; force_eltype=nothing, kwargs...)
         f = AtomsCalculators.forces(sys, calculator; kwargs...)
         @test typeof(f) <: AbstractVector
         @test eltype(f) <: AbstractVector
+        @test eltype(f) == ftype
+        @test typeof(f) == typeof(AtomsCalculators.zero_forces(sys,calculator))
         @test length(f) == length(sys)
         T = (eltype ∘ eltype)( f )
         f_matrix = reinterpret(reshape, T, f)
@@ -46,6 +48,7 @@ function test_forces(sys, calculator; force_eltype=nothing, kwargs...)
         f_nonallocating = zeros(ftype, length(sys))
         AtomsCalculators.forces!(f_nonallocating, sys, calculator; kwargs...)
         @test all( f_nonallocating .≈ f  )
+        fill!(f_nonallocating, zero(ftype)) # set f array to zeros
         AtomsCalculators.forces!(f_nonallocating, sys, calculator; dummy_kword659254=1, kwargs...)
         @test all( f_nonallocating .≈ f  )
         fc = AtomsCalculators.calculate(AtomsCalculators.Forces(), sys, calculator; kwargs...)
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,6 @@
 using AtomsBase
 using AtomsCalculators
+using StaticArrays
 using Test
 using Unitful
 
@@ -39,7 +40,8 @@ using AtomsCalculators.UntilityCalculators
         # or give extra information like pairlist
     
         # add your own definition
-        return AtomsCalculators.zero_forces(system, calculator)
+        f0 = SVector(1.0u"eV/Å", 1.0u"eV/Å", 1.0u"eV/Å")
+        return fill(f0, length(system))
     end
     
     AtomsCalculators.@generate_interface function AtomsCalculators.forces!(f::AbstractVector, system, calculator::MyOtherType; kwargs...)
@@ -118,34 +120,56 @@ end
 
 
 @testset "UntilityCalculators" begin
-
     hydrogen = isolated_system([
-    :H => [0, 0, 0.]u"Å",
-    :H => [0, 0, 1.]u"Å",
-    :H => [4., 0, 0.]u"Å",
-    :H => [4., 1., 0.]u"Å"
+        :H => [0, 0, 0.]u"Å",
+        :H => [0, 0, 1.]u"Å",
+        :H => [4., 0, 0.]u"Å",
+        :H => [4., 1., 0.]u"Å"
     ])
 
-    sub_cal = SubSystemCalculator(MyType(), 1:2)
+    @testset "SubSystemCalculator" begin
+        
+        sub_cal = SubSystemCalculator(MyType(), 1:2)
 
-    test_potential_energy(hydrogen, sub_cal)
-    test_forces(hydrogen, sub_cal)
-    test_virial(hydrogen, sub_cal)
+        test_potential_energy(hydrogen, sub_cal)
+        test_forces(hydrogen, sub_cal)
+        test_virial(hydrogen, sub_cal)
 
-    f = AtomsCalculators.zero_forces(hydrogen, sub_cal)
-    f_zero = f[1]
-    f_one = (ones ∘ typeof)( ustrip.(f_zero) ) * unit(f_zero[1])
-    fill!(f, f_one)
-    
+        f = AtomsCalculators.zero_forces(hydrogen, sub_cal)
+        f_zero = f[1]
+        f_one = (ones ∘ typeof)( ustrip.(f_zero) ) * unit(f_zero[1])
+        
 
-    @test AtomsCalculators.potential_energy(hydrogen, sub_cal) == 2.0u"eV"
+        @test AtomsCalculators.potential_energy(hydrogen, sub_cal) == 2.0u"eV"
 
-    AtomsCalculators.forces!(f, hydrogen, sub_cal)
-    @test f[1] == f_zero
-    @test f[2] == f_zero
-    @test f[3] == f_one
-    @test f[4] == f_one
+        AtomsCalculators.forces!(f, hydrogen, sub_cal)
+        @test f[1] == f_one
+        @test f[2] == f_one
+        @test f[3] == f_zero
+        @test f[4] == f_zero
 
-    v = AtomsCalculators.virial(hydrogen, sub_cal)
-    @test v[1,1] == 2.0u"eV"
+        v = AtomsCalculators.virial(hydrogen, sub_cal)
+        @test v[1,1] == 2.0u"eV"
+    end
+
+    @testset "CombinationCalculator" begin
+        
+        co_calc = CombinationCalculator(MyType(), MyType())
+
+        test_potential_energy(hydrogen, co_calc)
+        test_forces(hydrogen, co_calc)
+        test_virial(hydrogen, co_calc)
+        
+        e = AtomsCalculators.potential_energy(hydrogen, co_calc)
+        f = AtomsCalculators.forces(hydrogen, co_calc)
+        v = AtomsCalculators.virial(hydrogen, co_calc)
+        e_ref = 2* AtomsCalculators.potential_energy(hydrogen, MyType())
+        f_ref = 2* AtomsCalculators.forces(hydrogen, MyType())
+        v_ref = 2* AtomsCalculators.virial(hydrogen, MyType())
+        @test e ≈ e_ref
+        @test all( f .≈ f_ref )
+        @test all( v .≈ v_ref )
+    end
+
+
 end
